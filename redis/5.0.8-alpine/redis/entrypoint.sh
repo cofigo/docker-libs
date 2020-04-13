5902f0082830f6120dec0fb2320186fa94c87e6f
Endial Fang <endial@126.com>
2020-04-13 10:17:33 +0800

    [fix:redis] 修改默认配置文件存放目录；因使用gosu，所有相关文件都存放在对应的redis子目录中；增加初始化时创建相关目录及修改所属用户组操作；

--- redis/5.0.8-alpine/redis/entrypoint.sh
+++ redis/5.0.8-alpine/redis/entrypoint.sh
@@ -1,28 +1,42 @@
 #!/bin/sh
 # docker entrypoint script
 
-echo "[i] Initial Container"
+echo "[i] Initial Container for redis"
 
 set -e
+
+if [ ! -d /var/log/redis ]; then
+  echo "[i] Create directory: /var/log/redis"
+  mkdir -p /var/log/redis
+  chown redis:root /var/log/redis
+fi
+
+if [ ! -d /var/run/redis ]; then
+  echo "[i] Create directory: /var/run/redis"
+  mkdir -p /var/run/redis
+  chown redis:root /var/run/redis
+fi
 
 if [ ! -d /srv/data/redis ]; then
   echo "[i] Create directory: /srv/data/redis"
   mkdir -p /srv/data/redis
+  chown redis:root /srv/data/redis
 fi
 
 if [ ! -d /srv/conf/redis ]; then
   echo "[i] Create directory: /srv/conf/redis"
   mkdir -p /srv/conf/redis
+  chown redis:root /srv/conf/redis
 fi
 
 if [ ! -f /srv/conf/redis/redis.conf ]; then
   echo "[i] Copy default redis.conf to /srv/conf/redis"
-  cp /etc/redis/redis.conf /srv/conf/redis/
+  cp /etc/redis/default/redis.conf /srv/conf/redis/
 fi
 
 if [ ! -f /srv/conf/redis/sentinel.conf ]; then
   echo "[i] Copy default sentinel.conf to /srv/conf/redis"
-  cp /etc/redis/sentinel.conf /srv/conf/redis/
+  cp /etc/redis/default/sentinel.conf /srv/conf/redis/
 fi
 
 # first arg is `-f` or `--some-option`
